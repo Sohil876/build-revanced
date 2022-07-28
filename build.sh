c097313e539a2c189a8102a4c37141275e0c207d
Sohil876 <sohil876@gmail.com>
2022-07-28 15:27:32 +0530

    Seperate links into another file, build clean

--- build.sh
+++ build.sh
@@ -4,48 +4,39 @@
 #
 
 # Vars
+export GITHUB_TOKEN=${GHTOKEN}
 WRK_DIR=$(pwd)
-# Youtube apk
-Y_APK_URL="https://td.sohil876.workers.dev/0:/youtube_17.29.34.apk"
-Y_APK="youtube.apk"
-# Revanced patches
-R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.25.2/revanced-patches-2.25.2.jar"
+YOUTUBE_APK="youtube.apk"
 R_PATCHES="patches.jar"
-# Revanced integrations
-R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.28.1/app-release-unsigned.apk"
 R_INTEGRATIONS="integrations.apk"
-# Revanced patcher
-R_PATCHER_URL="https://github.com/revanced/revanced-patcher"
-# Revanced cli
-R_CLI_URL="https://github.com/revanced/revanced-cli"
 R_CLI="revanced-cli*-all.jar"
+source links.sh
 
 # Fetch files
 echo ""
 echo "Fetching files and repos..."
 echo ""
-wget ${Y_APK_URL} -O ${Y_APK}
+wget ${YOUTUBE_APK_URL} -O ${YOUTUBE_APK}
 wget ${R_PATCHES_URL} -O ${R_PATCHES}
 wget ${R_INTEGRATIONS_URL} -O ${R_INTEGRATIONS}
 git clone ${R_PATCHER_URL} patcher
 git clone ${R_CLI_URL} cli
 
 # Source build
-export GITHUB_TOKEN=${GHTOKEN}
 # Build patcher
 echo ""
 echo "Building patcher from source..."
 echo ""
 cd patcher
 chmod +x gradlew
-./gradlew publishToMavenLocal
+./gradlew clean publishToMavenLocal
 # Build cli
 echo ""
 echo "Building cli from source..."
 echo ""
 cd ${WRK_DIR}/cli
 chmod +x gradlew
-./gradlew build
+./gradlew clean build
 cp build/libs/*-all.jar ${WRK_DIR}/
 
 # Patch revanced
@@ -53,7 +44,7 @@ echo ""
 echo "Patching youtube apk..."
 echo ""
 cd ${WRK_DIR}
-java -jar ${R_CLI} -a ${Y_APK} -b ${R_PATCHES} -m ${R_INTEGRATIONS} -o revanced.apk -i enable-wide-searchbar -e force-vp9-codec -c 2>&1 | tee -a Patch.log
+java -jar ${R_CLI} -a ${YOUTUBE_APK} -b ${R_PATCHES} -m ${R_INTEGRATIONS} -o revanced.apk -i enable-wide-searchbar -e force-vp9-codec -c 2>&1 | tee -a Patch.log
 # Remove published patcher
 rm -rf ~/.m2/repository/app/revanced/*
 
---
+++ links.sh
@@ -0,0 +1,8 @@
+# File links
+YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/youtube_17.29.34.apk"
+R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.25.2/revanced-patches-2.25.2.jar"
+R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.28.1/app-release-unsigned.apk"
+# Git repos
+R_PATCHER_URL="https://github.com/revanced/revanced-patcher"
+R_CLI_URL="https://github.com/revanced/revanced-cli"
+
