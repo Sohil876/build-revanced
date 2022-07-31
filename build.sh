2bcac4a1238b92c589137a7a3df27a5922802e36
Sohil876 <sohil876@gmail.com>
2022-07-31 18:05:22 +0530

    Update patches & integrations, use prebuilt cli

--- build.sh
+++ build.sh
@@ -19,25 +19,28 @@ echo ""
 wget ${YOUTUBE_APK_URL} -O ${YOUTUBE_APK}
 wget ${R_PATCHES_URL} -O ${R_PATCHES}
 wget ${R_INTEGRATIONS_URL} -O ${R_INTEGRATIONS}
-git clone ${R_PATCHER_URL} patcher
-git clone ${R_CLI_URL} cli
+wget ${R_CLI_URL}
+#git clone ${R_PATCHER_GIT_URL} patcher
+#git clone ${R_CLI_GIT_URL} cli
 
 # Source build
 # Build patcher
-echo ""
-echo "Building patcher from source..."
-echo ""
-cd patcher
-chmod +x gradlew
-./gradlew clean publishToMavenLocal
+#echo ""
+#echo "Building patcher from source..."
+#echo ""
+#cd patcher
+#chmod +x gradlew
+#./gradlew clean publishToMavenLocal
 # Build cli
-echo ""
-echo "Building cli from source..."
-echo ""
-cd ${WRK_DIR}/cli
-chmod +x gradlew
-./gradlew clean build
-cp build/libs/*-all.jar ${WRK_DIR}/
+#echo ""
+#echo "Building cli from source..."
+#echo ""
+#cd ${WRK_DIR}/cli
+#chmod +x gradlew
+#./gradlew clean build
+#cp build/libs/*-all.jar ${WRK_DIR}/
+# Remove published patcher
+#rm -rf ~/.m2/repository/app/revanced/*
 
 # Patch revanced
 echo ""
@@ -45,6 +48,4 @@ echo "Patching youtube apk..."
 echo ""
 cd ${WRK_DIR}
 java -jar ${R_CLI} -a ${YOUTUBE_APK} -b ${R_PATCHES} -m ${R_INTEGRATIONS} -o revanced.apk -i enable-wide-searchbar -e force-vp9-codec -c 2>&1 | tee -a Patch.log
-# Remove published patcher
-rm -rf ~/.m2/repository/app/revanced/*
 
--- links.sh
+++ links.sh
@@ -1,8 +1,7 @@
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/youtube_17.29.34.apk"
-R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.25.2/revanced-patches-2.25.2.jar"
-R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.28.1/app-release-unsigned.apk"
-# Git repos
-R_PATCHER_URL="https://github.com/revanced/revanced-patcher"
-R_CLI_URL="https://github.com/revanced/revanced-cli"
-
+R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.28.0/revanced-patches-2.28.0.jar"
+R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.30.1/app-release-unsigned.apk"
+R_CLI_URL="https://github.com/revanced/revanced-cli/releases/download/v2.8.0/revanced-cli-2.8.0-all.jar"
+R_PATCHER_GIT_URL="https://github.com/revanced/revanced-patcher"
+R_CLI_GIT_URL="https://github.com/revanced/revanced-cli"
