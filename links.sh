a4e6f66ce7ead22bbd56f598450b8298d0c6bfd8
Sohil876 <sohil876@gmail.com>
2022-08-03 19:43:46 +0530

    Update cli, patches, integrations

--- links.sh
+++ links.sh
@@ -1,7 +1,7 @@
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/youtube_17.29.34.apk"
-R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.28.2/revanced-patches-2.28.2.jar"
+R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.30.1/revanced-patches-2.30.1.jar"
 R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.30.4/app-release-unsigned.apk"
-R_CLI_URL="https://github.com/revanced/revanced-cli/releases/download/v2.8.1/revanced-cli-2.8.1-all.jar"
+R_CLI_URL="https://github.com/revanced/revanced-cli/releases/download/v2.8.3/revanced-cli-2.8.3-all.jar"
 R_PATCHER_GIT_URL="https://github.com/revanced/revanced-patcher"
 R_CLI_GIT_URL="https://github.com/revanced/revanced-cli"
