2e308afbaba5ad79250d958cfcfc1222eda6db24
Sohil876 <sohil876@gmail.com>
2022-08-20 13:50:33 +0530

    Refactor, update patches
    Make specifying versions for prebuilt files easier
    Move git release prebuilt urls to build.sh, versions configurable in conf.sh
    Make more vars optional depending on build type

--- build.sh
+++ build.sh
@@ -4,18 +4,28 @@
 #
 
 # Vars (DO NOT CHANGE!)
+source conf.sh # Load conf file
 export GITHUB_TOKEN=${GHTOKEN}
 WRK_DIR=$(pwd)
 # File names
 YOUTUBE_APK="youtube.apk"
 R_PATCHES="patches.jar"
 R_INTEGRATIONS="integrations.apk"
-R_CLI="revanced-cli*-all.jar"
-# Git repos for source buids
-R_PATCHER_GIT_URL="https://github.com/revanced/revanced-patcher"
-R_CLI_GIT_URL="https://github.com/revanced/revanced-cli"
-# Load conf file
-source conf.sh
+R_CLI="revanced-cli-*-all.jar"
+# Prebuilt file links, configure versions in conf.sh file
+R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v${R_PATCHES_VERSION}/revanced-patches-${R_PATCHES_VERSION}.jar"
+R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v${R_INTEGRATIONS_VERSION}/app-release-unsigned.apk"
+if [ "${SOURCE_BUILD_CLI}" = false ] || [ "${SOURCE_BUILD_CLI}" = False ] || [ "${SOURCE_BUILD_CLI}" = FALSE ]; then
+  R_CLI_URL="https://github.com/revanced/revanced-cli/releases/download/v${R_CLI_VERSION}/revanced-cli-${R_CLI_VERSION}-all.jar"
+elif [ "${SOURCE_BUILD_CLI}" = true ] || [ "${SOURCE_BUILD_CLI}" = True ] || [ "${SOURCE_BUILD_CLI}" = TRUE ]; then
+  # Git repos for source buids
+  R_PATCHER_GIT_URL="https://github.com/revanced/revanced-patcher"
+  R_CLI_GIT_URL="https://github.com/revanced/revanced-cli"
+else
+  echo "Invalid value in SOURCE_BUILD_CLI var!"
+  echo "Allowed values true/True/TRUE or false/False/FALSE"
+  exit 1
+fi
 
 # Fetch files
 echo ""
--- conf.sh
+++ conf.sh
@@ -3,15 +3,18 @@
 #
 
 # Vars
-# Explictly include/exclude patches
-INCLUDE_PATCHES="" # -i patchname
-EXCLUDE_PATCHES="" # -e patchname
 # Build revanced cli/patcher from source and use it instead of prebuilts
 SOURCE_BUILD_CLI="false"
 
+# Explictly include/exclude patches
+INCLUDE_PATCHES="" # -i patchname
+EXCLUDE_PATCHES="" # -e patchname
+
+# Versions to fetch files for prebuilt from github releases
+R_PATCHES_VERSION="2.39.0"
+R_INTEGRATIONS_VERSION="0.33.1"
+R_CLI_VERSION="2.9.3"
+
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/youtube_17.29.34.apk"
-R_PATCHES_URL="https://github.com/revanced/revanced-patches/releases/download/v2.37.0/revanced-patches-2.37.0.jar"
-R_INTEGRATIONS_URL="https://github.com/revanced/revanced-integrations/releases/download/v0.33.1/app-release-unsigned.apk"
-R_CLI_URL="https://github.com/revanced/revanced-cli/releases/download/v2.9.3/revanced-cli-2.9.3-all.jar"
 
