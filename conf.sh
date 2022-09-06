ff6285a72525acba0dccc28121d983dea2db5574
Sohil876 <sohil876@gmail.com>
2022-09-07 04:44:10 +0530

    Update patches

--- conf.sh
+++ conf.sh
@@ -11,7 +11,7 @@ INCLUDE_PATCHES="" # -i patchname
 EXCLUDE_PATCHES="" # -e patchname
 
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.50.2"
+R_PATCHES_VERSION="2.50.4"
 R_INTEGRATIONS_VERSION="0.36.5"
 R_CLI_VERSION="2.9.5"
 
