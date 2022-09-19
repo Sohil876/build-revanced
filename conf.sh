cf98678e90c7c4334c39f72a56b9a9e491139873
Sohil876 <sohil876@gmail.com>
2022-09-20 03:09:25 +0530

    Bump youtube version, disable manager build for now

--- conf.sh
+++ conf.sh
@@ -5,7 +5,7 @@
 # Vars
 # Build from source and use it instead of prebuilts
 SOURCE_BUILD_CLI="false"
-SOURCE_BUILD_MANAGER="true"
+SOURCE_BUILD_MANAGER="false"
 
 # Explictly include/exclude patches
 INCLUDE_PATCHES="" # -i patchname
@@ -17,5 +17,5 @@ R_INTEGRATIONS_VERSION="0.37.4"
 R_CLI_VERSION="2.10.2"
 
 # File links
-YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/ReVanced/youtube_17.33.42.apk"
+YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/ReVanced/youtube_17.36.37.apk"
 
