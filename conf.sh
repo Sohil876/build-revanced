6a8e05fef144ac060b9af66eea0efa0c1f6fdca7
Sohil876 <sohil876@gmail.com>
2022-09-23 20:49:19 +0530

    Update cli, patches and integrations

--- conf.sh
+++ conf.sh
@@ -7,9 +7,9 @@ SOURCE_BUILD_MANAGER="false"
 INCLUDE_PATCHES="" # -i patchname
 EXCLUDE_PATCHES="" # -e patchname
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.66.2"
-R_INTEGRATIONS_VERSION="0.41.1"
-R_CLI_VERSION="2.11.1"
+R_PATCHES_VERSION="2.68.2"
+R_INTEGRATIONS_VERSION="0.42.1"
+R_CLI_VERSION="2.11.2"
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/0:/ReVanced/youtube_17.36.37.apk"
 ###
