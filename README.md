059e4b5b4fa2bfcfd4d86f2890a771d035c3009e
Sohil876 <sohil876@gmail.com>
2023-02-13 02:40:51 +0530

    Bump youtube version, update patches, integrations

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![Build](https://github.com/Sohil876/build-revanced/actions/workflows/build.yaml/badge.svg?branch=main&event=push)](https://github.com/Sohil876/build-revanced/actions)
 
 ### Non-Root Only
-   - Youtube : [17.49.37](https://www.apkmirror.com/apk/google-inc/youtube/youtube-17-49-37-release/)
+   - Youtube : [18.03.36](https://www.apkmirror.com/apk/google-inc/youtube/youtube-18-03-36-release/)
--- conf.sh
+++ conf.sh
@@ -7,10 +7,10 @@ SOURCE_BUILD_MANAGER="false" # (Compose)
 INCLUDE_PATCHES="" # -i patchname
 EXCLUDE_PATCHES="" # -e patchname
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.159.0"
-R_INTEGRATIONS_VERSION="0.95.0"
+R_PATCHES_VERSION="2.161.1"
+R_INTEGRATIONS_VERSION="0.96.1"
 R_CLI_VERSION="2.20.0"
-Y_APK_VERSION="17.49.37"
+Y_APK_VERSION="18.03.36"
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/1:/ReVanced/youtube_${Y_APK_VERSION}.apk"
 ###
