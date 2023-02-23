0a71b1506c5c32e155b217bc14ecb31127eae451
Sohil876 <sohil876@gmail.com>
2023-02-24 02:45:30 +0530

    Update patches, integrations

--- conf.sh
+++ conf.sh
@@ -7,8 +7,8 @@ SOURCE_BUILD_MANAGER="false" # (Compose)
 INCLUDE_PATCHES="" # -i patchname
 EXCLUDE_PATCHES="" # -e patchname
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.161.1"
-R_INTEGRATIONS_VERSION="0.96.1"
+R_PATCHES_VERSION="2.163.0"
+R_INTEGRATIONS_VERSION="0.98.0"
 R_CLI_VERSION="2.20.0"
 Y_APK_VERSION="18.03.36"
 # File links
