019e407162f5186669d2bb2a17537757126d074f
Sohil876 <sohil876@gmail.com>
2023-09-03 00:21:36 +0530

    Bump youtube version, update cli, patches, integrations

--- conf.sh
+++ conf.sh
@@ -7,10 +7,10 @@ SOURCE_BUILD_MANAGER="false" # (Compose)
 INCLUDE_PATCHES="" # -i patchname
 EXCLUDE_PATCHES="" # -e patchname
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.187.0"
-R_INTEGRATIONS_VERSION="0.115.1"
-R_CLI_VERSION="2.22.0"
-Y_APK_VERSION="18.23.35"
+R_PATCHES_VERSION="2.189.0"
+R_INTEGRATIONS_VERSION="0.117.0"
+R_CLI_VERSION="3.1.0"
+Y_APK_VERSION="18.32.39"
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/1:/ReVanced/youtube_${Y_APK_VERSION}.apk"
 ###
