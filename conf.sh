f1ddcef7d91eb97b0dc993aa1b9cd18d9e0f5c70
Sohil876 <sohil876@gmail.com>
2023-11-26 19:16:31 +0530

    Bump youtube version, update cli, patches, integrations

--- conf.sh
+++ conf.sh
@@ -7,10 +7,10 @@ SOURCE_BUILD_MANAGER="false" # (Compose)
 INCLUDE_PATCHES="-i custom-branding" # -i some-patch-name, typically its same as patchname on revanced git/site just all lowercase with space replaced by "-" symbol.
 EXCLUDE_PATCHES="" # -e some-patch-name, typically its same as patchname on revanced git/site just all lowercase with space replaced by "-" symbol.
 # Versions to fetch files for prebuilt from github releases
-R_PATCHES_VERSION="2.197.0"
-R_INTEGRATIONS_VERSION="0.122.0"
-R_CLI_VERSION="4.1.0"
-Y_APK_VERSION="18.38.44"
+R_PATCHES_VERSION="2.201.0"
+R_INTEGRATIONS_VERSION="0.124.0"
+R_CLI_VERSION="4.2.0"
+Y_APK_VERSION="18.45.41"
 # File links
 YOUTUBE_APK_URL="https://td.sohil876.workers.dev/1:/ReVanced/youtube_${Y_APK_VERSION}.apk"
 ###
